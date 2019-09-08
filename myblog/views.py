681b684f1a77a3180af2ee547684267df74a14ad
annakonior <annakonior91@gmail.com>
2019-09-08 11:16:46 +0200

    Modified templates to display posts from database

--- myblog/views.py
+++ myblog/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Post
+def post_list(request): #request-wszystko co otrzymujemy od użytkowanika internetu
+    posts=Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date') #posts to nazwa QuerySetu
+    return render(request, 'myblog/post_list.html', {'posts':posts})
+#{} jest miejscem, w którym możemy dodać parę rzeczy do wykorzystania w szablonie
 
-# Create your views here.
-def post_list(request):
-    return render(request, 'myblog/post_list.html', {})
+
