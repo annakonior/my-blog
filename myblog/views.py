bb7215990f0bfbf8cdecae0943f038211a6952d7
annakonior <annakonior91@gmail.com>
2019-09-08 15:52:12 +0200

    Added view and template for detailed blog post as well as CSS for the site.

--- myblog/views.py
+++ myblog/views.py
@@ -1,3 +1,4 @@
+from django.shortcuts import render, get_object_or_404
 from django.shortcuts import render
 from django.utils import timezone
 from .models import Post
@@ -5,5 +6,6 @@ def post_list(request): #request-wszystko co otrzymujemy od użytkowanika intern
     posts=Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date') #posts to nazwa QuerySetu
     return render(request, 'myblog/post_list.html', {'posts':posts})
 #{} jest miejscem, w którym możemy dodać parę rzeczy do wykorzystania w szablonie
-
-
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'myblog/post_detail.html', {'post': post})
